adf71792b492f5493bb0bf3a395d3861a98dad0d
Bashu <bawasthi089@gmail.com>
2025-08-15 07:50:46 +0545

    url_updated

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 connectDB()
 connectCloudinary()
+
+const allowedOrigins =[' http://localhost:5173',' http://localhost:5174', 'https://prescripto-frontend-tau.vercel.app']
+
 // Middleware 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:allowedOrigins , credentials:true}));
 
 // API endpoints
 
